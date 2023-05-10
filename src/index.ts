301368082fdb00893f224fc3bc4e2d9ab4ebecd1
Taher Jerbi <jerbi.taherr@gmail.com>
2023-05-10 22:50:56 +0100

    feat: listen for new batch results

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import kafka, { Message } from 'kafka-node';
 import WebSocket from 'ws';
 
+type BatchUpdate = {
+    [sentiment in Sentiment]?: number;
+}
+type SocketMessage = {
+    type: "new_comment";
+    data: Comment[];
+} | {
+    type: "batch_update";
+    data: BatchUpdate;
+}
+
 const client = new kafka.KafkaClient({kafkaHost: 'kafka:9092', connectTimeout: 1000, requestTimeout: 1000});
 
 // retry to connect when kafka is down
-const consumer = new kafka.Consumer(client, [{ topic: 'reddit-comments-sentiments', partition: 0 }], { autoCommit: false });
+const consumer = new kafka.Consumer(client, [{ topic: 'reddit-comments-sentiments', partition: 0 }, { topic: "batch_result", partition: 0 }], { autoCommit: false });
 
 const wss = new WebSocket.Server({ port: 4000 });
 const comments: Map<String, Comment> = new Map();
@@ -18,16 +29,11 @@ wss.on('connection', function connection(ws: WebSocket) {
     });
 });
 consumer.on('message', function (message: Message) {
-    const comment = parseComment(message.value.toString());
-    comments.set(comment.comment_id, comment);
-    console.log('received comment: %s', comment.comment_id);
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            // send last 6 comments
-            client.send(JSON.stringify(Array.from(comments.values()).slice(-6)));
-        }
+    if(message.topic === "reddit-comments-sentiments") {
+        handleComment(message.value.toString());
+    } else if(message.topic === "batch_result") {
+        handleBatchResult(message.value.toString());
     }
-    );
 });
 
 // comments_id, parent_id, body, subreddit, timestamp, SENTIMENT
@@ -41,6 +47,17 @@ type Comment = {
 }
 type Sentiment = "Positive" | "Negative" | "Neutral" | "Unknown";
 
+
+function handleComment(message: string) {
+    const comment = parseComment(message);
+    comments.set(comment.comment_id, comment);
+    console.log('received comment: %s', comment.comment_id);
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "new_comment", data: Array.from(comments.values()).slice(-6) }));
+        }
+    });
+}
 function parseComment(comment: string): Comment {
     const comment_parts = comment.split(',');
     const comment_id = comment_parts[0];
@@ -59,4 +76,34 @@ function parseComment(comment: string): Comment {
         timestamp,
         sentiment
     }
+}
+
+function handleBatchResult(message: string) {
+    const batchUpdate = parseBatchResult(message);
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "batch_update", data: batchUpdate }));
+        }
+    });
+}
+
+// [(Positive, 1), (Negative, 2), (Neutral, 3), (Unknown, 4)]
+function parseBatchResult(message: string): BatchUpdate {
+    const batchUpdate: BatchUpdate = {};
+
+    // remove brackets
+    const batchResult = message.slice(1, message.length - 1);
+    // remove spaces
+    const batchResultNoSpaces = batchResult.replace(/\s/g, '');
+    // split by comma
+    const batchResultParts = batchResultNoSpaces.split(',');
+    for(let i = 0; i < batchResultParts.length; i += 2) {
+        const sentiment = batchResultParts[i].slice(1, batchResultParts[i].length) as Sentiment;
+        const count = parseInt(batchResultParts[i + 1].slice(0, batchResultParts[i + 1].length - 1));
+        batchUpdate[sentiment] = count;
+    }
+    
+    console.log('received batch result: %s', JSON.stringify(batchUpdate));
+    
+    return batchUpdate;
 }
